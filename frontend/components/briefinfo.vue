b411be9340a0ff29e51030497ba3299ad34fe8ed
tu-saeki <t-saeki@nttr.co.jp>
2021-05-23 15:32:52 +0900

    feat: トイレ検索表示機能のトップ画面のコンポーネントを作成

---
+++ frontend/components/briefinfo.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+    <v-card-title>【1】<a href="#">グランパーク田町</a></v-card-title>
+    <v-card-text>
+      <div class="ml-1">6F、7F、8F</div>
+      <div class="ml-1">やや混んでいます</div>
+    </v-card-text>
+  </v-card>
+</template>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,17 +1,27 @@
 <template>
-  <review></review>
+  <!--<review></review>-->
+  <!--<briefinfo></briefinfo>-->
+  <!--<briefinfolist></briefinfolist>-->
+  <!--<overview></overview>-->
+  <floor></floor>
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
 import VuetifyLogo from '~/components/VuetifyLogo.vue'
 import Review from '~/components/review.vue'
+import BriefInfoList from '~/components/briefinfo_list.vue'
+import Overview from '~/components/evlov.vue'
+import Floor from '~/components/evlflr.vue'
 
 export default {
   components: {
     Logo,
     VuetifyLogo,
     Review,
+    BriefInfoList,
+    Overview,
+    Floor,
   },
 }
 </script>
