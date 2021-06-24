74e60f75e7471f3508bb7a7c073850583b772cf2
hmori52 <h.mori@nttr.co.jp>
2021-06-24 18:55:40 +0900

    refactor: v-forを利用

--- frontend/pages/main.vue
+++ frontend/pages/main.vue
@@ -13,18 +13,11 @@
 
     <div style="display: block; width: 100%; height: 85%; font-size: 0; background: black;">
       <div style="display: inline-block; width: 25%; height: 100%; font-size: 0; background: black;">
-        <briefinfo></briefinfo>
-        <briefinfo></briefinfo>
-        <briefinfo></briefinfo>
-        <briefinfo></briefinfo>
-        <briefinfo></briefinfo>
-        <briefinfo></briefinfo>
-        <briefinfo></briefinfo>
+        <briefinfo v-for="n of 7" :key="n"></briefinfo>
       </div>
       <div style="display: inline-block; font-size: 14px; width: 25%; height: 100%; background: black; vertical-align: top;">
         <overview></overview>
-        <floor></floor>
-        <floor></floor>
+        <floor v-for="n of 2" :key="n"></floor>
       </div>
       <div style="display: inline-block; font-size: 12px; width: 50%; height: 100%; vertical-align: top;">
         <img src="map.png" alt="map" title="map">
